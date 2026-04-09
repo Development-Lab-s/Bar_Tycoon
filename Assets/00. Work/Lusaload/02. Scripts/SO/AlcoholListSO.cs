2f68c7099b0f6eaf3d35513c526cf9d2dc2327a8
lusalord <lusalord63@gmail.com>
2026-04-09 17:50:05 +0900

    feat : Create Alcohol Button

---
+++ Assets/00. Work/Lusaload/02. Scripts/SO/AlcoholListSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _00._Work.Lusaload._02._Scripts.SO
+{
+    [CreateAssetMenu(fileName = "Alcohol List SO", menuName = "Alcohol/Alcohol List", order = 0)]
+    public class AlcoholListSO : ScriptableObject
+    {
+        public List<BaseAlcoholDataSO> alcoholDataList;
+    }
+}
