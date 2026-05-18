13a519747a60982280a84bb011dc66f8af25efda
woosungtae <a01076755795@gmail.com>
2026-05-19 00:14:08 +0900

    fix : achieveSlot

--- Assets/00. Work/Goat/02. Scripts/UI/UpgradeUI/UpgradeSlot/UpgradeSlotUI.cs
+++ Assets/00. Work/Goat/02. Scripts/UI/UpgradeUI/UpgradeSlot/UpgradeSlotUI.cs
@@ -50,7 +50,7 @@ namespace _00._Work.Goat._02._Scripts.UI.UpgradeUI.UpgradeSlot
             costText.text = data.GetCost();
             nowLevel.text = data.GetLevel(0);
             nextLevel.text = data.GetLevel(1);
-            nowStat.text = (data.UpgradeDataSo.TargetStat.BaseValue + data.GetTotalIncreaseValue() ).ToString();
+            nowStat.text = (data.UpgradeDataSo.TargetStat.BaseValue + data.GetTotalIncreaseValue()).ToString();
             nextStat.text = (data.UpgradeDataSo.TargetStat.BaseValue + data.GetTotalIncreaseValue() + data.UpgradeDataSo.IncreaseValue).ToString();
         }
     }
