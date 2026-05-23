8eb44bb3538e4c1e8a3cf65b549b4d6b88fdb926
woosungtae <a01076755795@gmail.com>
2026-05-24 00:57:26 +0900

    feat : FurnitureDictionary

---
+++ Assets/00. Work/Goat/02. Scripts/UI/DictonaryUI/Codex/FurnitureSlotUI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using _00._Work.Lusaload._02._Scripts.SO;
+using BBJ.GridSystem.Objects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _00._Work.Goat._02._Scripts.UI.DictonaryUI.Codex
+{
+    public class FurnitureSlotUI : MonoBehaviour
+    {
+        [Header("UI")]
+        [SerializeField] private Image objectImage;
+     
+        public event Action<ObjectDataSO> OnClickBtn;
+        
+        private ObjectDataSO _objectSo;
+
+        private void Awake()
+        {
+            objectImage.gameObject.SetActive(false);
+        }
+
+        public void InputSO(ObjectDataSO objectSlotSo)
+        {
+            objectImage.gameObject.SetActive(true);
+            _objectSo = objectSlotSo;
+            objectImage.sprite = objectSlotSo.Icon;
+        }
+        
+        public void Clear()
+        {
+            _objectSo = null;
+            objectImage.sprite = null;
+            objectImage.gameObject.SetActive(false);
+        }
+
+        public void BtnClick()
+        {
+            if (_objectSo != null)
+            {
+                OnClickBtn?.Invoke(_objectSo);   
+            }
+        }
+    }
+}
