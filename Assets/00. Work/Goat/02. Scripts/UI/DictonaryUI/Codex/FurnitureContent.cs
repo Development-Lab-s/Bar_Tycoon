2d96091c81a95a1068fc0d3d9e25e3baddc7d665
afw246 <chqwulmin0814@gmail.com>
2026-05-24 03:29:43 +0900

    etc

---
+++ Assets/00. Work/Goat/02. Scripts/UI/DictonaryUI/Codex/FurnitureContent.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _00._Work.Lusaload._02._Scripts.SO;
+using BBJ.GridSystem.Objects;
+using UnityEngine;
+
+namespace _00._Work.Goat._02._Scripts.UI.DictonaryUI.Codex
+{
+    public class FurnitureContent : MonoBehaviour
+    {
+        [SerializeField] private FurnitureSlotUI cockTailSlotPrefab;
+        
+        private List<FurnitureSlotUI>  _slots = new();
+        
+        public event Action<ObjectDataSO>  OnClickBtn; 
+        
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void OnDestroy()
+        {
+            foreach (FurnitureSlotUI slot in _slots)
+            {
+                slot.OnClickBtn -= HandleOnClick;
+            }
+        }
+
+        private void Init()
+        {
+            _slots = GetComponentsInChildren<FurnitureSlotUI>().ToList();
+
+            foreach (FurnitureSlotUI slot in _slots)
+            {
+                slot.OnClickBtn += HandleOnClick;
+            }
+        }
+        
+        public void SetView(IReadOnlyCollection<ObjectDataSO> objectDataList)
+        {
+            List<ObjectDataSO> list = objectDataList.ToList();
+            
+            while (objectDataList.Count > _slots.Count)
+            {
+                FurnitureSlotUI cockTailSlot = Instantiate(cockTailSlotPrefab, transform);
+                cockTailSlot.OnClickBtn += HandleOnClick;
+                _slots.Add(cockTailSlot);
+            }
+            
+            for (int i = 0; i < _slots.Count; i++)
+            {
+                if (i < list.Count)
+                {
+                    _slots[i].InputSO(list[i]);
+                }
+                else
+                {
+                    _slots[i].Clear();
+                }
+            }
+        }
+
+        private void HandleOnClick(ObjectDataSO obj)
+        {
+            OnClickBtn?.Invoke(obj);
+        }
+    }
+}
