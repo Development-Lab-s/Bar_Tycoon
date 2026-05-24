3dbb6b74c50c7dd45e11c387c03a8dac4352c6b6
afw246 <chqwulmin0814@gmail.com>
2026-05-25 03:59:46 +0900

    튜토리얼 2번까지 완성

--- Assets/00. Work/PCM/02. Scripts/Toturial/SO/TuturialDatas.cs
+++ Assets/00. Work/PCM/02. Scripts/Toturial/SO/TuturialDatas.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TuturialDatas", menuName = "Scriptable Objects/TuturialDatas")]
 public class TuturialDatas : ScriptableObject
 {
-   public string TutorialName;
-   public ToturialInfoSO[] data;   
+    public bool isEnd;
+    public string TutorialName;
+    public ToturialInfoSO[] data;   
 }
