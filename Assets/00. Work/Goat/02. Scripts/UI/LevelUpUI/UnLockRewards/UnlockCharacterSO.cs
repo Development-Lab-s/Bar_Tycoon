97eb41646af578c803fc16fd2a65e6b5f689ca10
woosungtae <a01076755795@gmail.com>
2026-05-24 21:41:15 +0900

    feat : characterAdd

---
+++ Assets/00. Work/Goat/02. Scripts/UI/LevelUpUI/UnLockRewards/UnlockCharacterSO.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using _00._Work.Goat._02._Scripts.Events;
+using BBJ.EventSystem;
+using BBJ.Staff;
+using Gamelib.EventSystem;
+using UnityEngine;
+
+namespace _00._Work.Goat._02._Scripts.UI.LevelUpUI.UnLockRewards
+{
+    [CreateAssetMenu(fileName = "unlockCharacter", menuName = "SO/unlock/Character", order = 0)]
+    public class UnlockCharacterSO : AbstractUnlockSO
+    {
+        [SerializeField] private EventChannelSO cameraManagerEvent;
+        [SerializeField] private Sprite characterSprite; 
+        [SerializeField]private StaffConfigSO _staffConfig;
+        
+        public readonly List<Vector2> spawnPositions = new List<Vector2>();
+        public override void LevelUpReward()
+        {
+            spawnPositions.Clear();
+            eventChannelSo?.RaiseEvent(new StaffSpawnEvent().Init(_staffConfig, (pos) => spawnPositions.Add(pos)));
+            cameraManagerEvent.RaiseEvent(new CameraManagerEvent().Init(spawnPositions, false));
+        }
+
+        public override List<Sprite> GetSprite()
+        {
+            List<Sprite> sprites = new List<Sprite>();
+            sprites.Add(characterSprite);
+            return  sprites;
+        }
+    }
+}
