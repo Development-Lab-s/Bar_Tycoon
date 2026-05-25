41e0510705663e6bd2e2fc32032ce0b505e60a6c
woosungtae <a01076755795@gmail.com>
2026-05-26 06:08:51 +0900

    feat : particle, SceneMove

--- Assets/00. Work/BBJ/02. Scripts/Scene/MainSceneHost.cs
+++ Assets/00. Work/BBJ/02. Scripts/Scene/MainSceneHost.cs
@@ -9,6 +9,13 @@ namespace BBJ.Scene
         public SceneType SceneType => SceneType.Main;
         public Camera sceenMainCamera;
         public CameraManager cameraManager;
+        
+        [Header("Canvas")]
+        [SerializeField] private Canvas[] mainCanvases;
+        [SerializeField] private int foregroundOffset = 0;
+        [SerializeField] private int backgroundOffset = -1000;
+        
+        private int[] _originalOrders;
 
         private void Awake()
         {
@@ -17,6 +24,8 @@ namespace BBJ.Scene
             cameraManager = FindFirstObjectByType<CameraManager>();
             sceenMainCamera?.gameObject.SetActive(false);
             cameraManager?.gameObject.SetActive(false);
+            
+            SaveOriginalCanvasOrders();
         }
 
         public void OnForeground()
@@ -24,11 +33,48 @@ namespace BBJ.Scene
             sceenMainCamera?.gameObject.SetActive(true);
             cameraManager?.gameObject.SetActive(true);
             Camera.SetupCurrent(sceenMainCamera);
+            
+            RestoreCanvasOrders();
         }
         public void OnBackground()
         {
             cameraManager?.gameObject.SetActive(false);
             sceenMainCamera?.gameObject.SetActive(false);
+            
+            LowerCanvasOrders();
+        }
+        
+        private void SaveOriginalCanvasOrders()
+        {
+            _originalOrders = new int[mainCanvases.Length];
+
+            for (int i = 0; i < mainCanvases.Length; i++)
+            {
+                if (mainCanvases[i] == null) continue;
+                _originalOrders[i] = mainCanvases[i].sortingOrder;
+            }
+        }
+
+        private void RestoreCanvasOrders()
+        {
+            for (int i = 0; i < mainCanvases.Length; i++)
+            {
+                if (mainCanvases[i] == null) continue;
+
+                mainCanvases[i].overrideSorting = true;
+                mainCanvases[i].sortingOrder = _originalOrders[i] + foregroundOffset;
+            }
+        }
+
+        private void LowerCanvasOrders()
+        {
+            for (int i = 0; i < mainCanvases.Length; i++)
+            {
+                if (mainCanvases[i] == null) continue;
+
+                mainCanvases[i].overrideSorting = true;
+                mainCanvases[i].sortingOrder = _originalOrders[i] + backgroundOffset;
+            }
         }
     }
 }
---
+++ Assets/00. Work/Goat/02. Scripts/Module/AgentUpgraderModule.cs
@@ -0,0 +1,36 @@
+﻿using _00._Work._Resources._02._Scripts.Modules;
+using _00._Work.Goat._02._Scripts.Events;
+using Agents.StatSystem;
+using Gamelib.EventSystem;
+using UnityEngine;
+
+namespace _00._Work.Goat._02._Scripts.Module
+{
+    public class AgentUpgraderModule : MonoBehaviour, IModule, IAfterInitModule
+    {
+        [SerializeField] private EventChannelSO upgradeChannel;
+        private IStatModule _statModule;
+        private ModuleOwner _owner;
+        public void Initialize(ModuleOwner owner)
+        {
+            _owner = owner;
+            upgradeChannel.AddListener<UpgradeEvent>(HandleUpgradeEvent);
+        }
+        
+        public void AfterInit()
+        {
+            _statModule = _owner.GetModule<IStatModule>();   
+        }
+
+        private void OnDestroy()
+        {
+            upgradeChannel.RemoveListener<UpgradeEvent>(HandleUpgradeEvent);
+        }
+
+        private void HandleUpgradeEvent(UpgradeEvent obj)
+        {
+            _statModule.RemoveModifier(obj.statSo.AssetIndex, this);
+            _statModule.AddModifier(obj.statSo.AssetIndex, this, obj.amount);
+        }
+    }
+}
