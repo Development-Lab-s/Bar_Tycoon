8bf70c77064374698eb407db628e9d4a605c297e
CheolYee <mincheol2008@gmail.com>
2026-05-26 14:17:33 +0900

    fix : Char Likeability

--- Assets/00. Work/PCM/02. Scripts/Contract/TestContractObject.cs
+++ Assets/00. Work/PCM/02. Scripts/Contract/TestContractObject.cs
@@ -6,13 +6,15 @@ using UnityEngine.Events;
 
 namespace Assets._00._Work.PCM._02._Scripts
 {
-    public class TestContractObject : MonoBehaviour, IContractObject, IModule, IHoverable
+    public class TestContractObject : MonoBehaviour, IContractObject, IModule, IHoverable, ILikeabillty
     {
         private ModuleOwner _owner;
         private IHoverRenderer _hoverRenderer;
         private bool isHover;
         public bool CanInteracting { get; set; }
         [field: SerializeField] public UnityEvent OnClickEvent { get; set; }
+        [field: SerializeField] public CharacterEnum characterEnum { get; set; }
+        [field: SerializeField] public UnityEvent<int> OnLike { get; set; }
 
         public virtual void Initialize(ModuleOwner owner)
         {
