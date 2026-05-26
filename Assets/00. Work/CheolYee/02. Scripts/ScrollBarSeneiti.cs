d16278321f8978e000250f5ff8928e0d3eadc4be
CheolYee <mincheol2008@gmail.com>
2026-05-27 08:23:58 +0900

    bugfix

---
+++ Assets/00. Work/CheolYee/02. Scripts/ScrollBarSeneiti.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _00._Work.CheolYee._02._Scripts
+{
+    public class ScrollBarSeneiti : MonoBehaviour
+    {
+        [SerializeField] ScrollRect scrollbar;
+
+        private void Start()
+        {
+            if (scrollbar != null)
+            {
+                scrollbar.scrollSensitivity = 12;
+            }
+        }
+    }
+}
