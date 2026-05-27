e4f6fabbd1496cd4636d18c3dd2af8bba8429e9c
afw246 <chqwulmin0814@gmail.com>
2026-05-27 12:11:23 +0900

    asd

--- Assets/00. Work/PCM/02. Scripts/Contract/AbstructContractObject.cs
+++ Assets/00. Work/PCM/02. Scripts/Contract/AbstructContractObject.cs
@@ -1,10 +1,13 @@
 using _00._Work._Resources._02._Scripts.Modules;
 using Assets._00._Work.PCM._02._Scripts;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace _00._Work.PCM._02._Scripts
 {
-    public abstract class AbstructContractObject : TestContractObject
+    public abstract class AbstructContractObject : TestContractObject, ILikeabillty
     {
+        [field: SerializeField] public CharacterEnum characterEnum { get; set; }
+        [field: SerializeField] public UnityEvent<int> OnLike { get; set; }
     }
 }
